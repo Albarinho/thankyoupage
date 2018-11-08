595b88e170d77d3fef27b61879c477eeb7e2be88
albaro claros <ac484@njit.edu>
2018-11-08 01:58:26 -0500

    finish the thank you message

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,9 +4,6 @@
 
 
     <h1>About Page</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-
-
 
 
 @endsection
